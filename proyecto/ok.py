6bdbd50ef32ff401be5cb5ecf2489e6bd8074b7c
Gonzalo Maximiliano Navarreta <navarreta22gonza@gmail.com>
2025-05-21 16:32:06 -0300

    libreria random

---
+++ proyecto/ok.py
@@ -0,0 +1,32 @@
+import random
+# numero= int (input("ingresa tu numero"))
+# nombre1,nombre2= nombre.split(',')
+# ##print (nombre1)
+# print (nombre2)
+
+# lista=[]
+
+# lista.append(54)
+
+# print (lista)
+
+# i= int(input("ingrese su numero"))
+# numero =1
+# while numero >= i:
+#     if i % numero == 0 :
+#         contadordediv = contadordediv + 1
+#         numero = numero + 1
+
+# if contadordediv <= 3 :
+#     print (F"el {i}")
+
+# lista=[]
+# k= random.randint(0,20)
+# while k != 0:
+#     lista.append(random.randint(1,100))
+#     k= random.randint(0,20)
+
+# print(f"{lista}")
+
+numero= random.uniform(0,100.0)
+print (numero)
